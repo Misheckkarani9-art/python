987238bdadfba8076addb6ffb8add6dd6efb44b4
Misheck Karani <meshackkarani9@gmail.com>
2026-02-05 00:30:24 +0300

    new

---
+++ assignment.py
@@ -0,0 +1,18 @@
+# create a python program that is able to calculate the BMI of a person whose weight of78kgs and a height of 1.75
+Weight =78
+height =1.75
+BMI=(Weight)/(height)
+print("the BMI is" ,BMI)
+
+
+#area
+length =48
+Width=25
+Area= length*Width
+print("the area is" ,Area)
+
+#Perimeter
+length =48
+Width =25
+Perimeter=length + Width
+print("the perimmeter is " ,Perimeter)
---
+++ lesson1d.py
@@ -0,0 +1,11 @@
+#Create  apython program that is able to calculate the simpple interst given the principle as 5000, rate as 5% and time 8 years .  si=(p*r*t)/100
+principle =5000
+rate = 5/100
+time =8
+SimpleInterest =(principle * rate * time)/100
+print("the simple interest is" ,SimpleInterest)
+
+
+#Assignment 
+
+# 1. Write a python program that is able to calculate the BMI of a person whose weight is 78kgs and height is 1.75 meters. BMI =(weight ) / (height)
