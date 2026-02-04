80411518c4968e888935ec7e290f016b364f1540
Misheck Karani <meshackkarani9@gmail.com>
2026-02-04 14:00:15 +0300

    new

---
+++ lesson1b.py
@@ -0,0 +1,19 @@
+# python Data types
+#String - itis a seeries  of characters that are ooonside of quotes
+name ="misheck karani"
+print(name)
+print (type(name))
+
+# numeric 
+# IUnt -This indicates a whole nummber in python . For an interger ,we dont use quotes
+
+age =39
+print("The age of a person is: " ,age)
+print(type(age))
+
+# Float : ias is a number with a decimal
+coordinates =16.34
+print("the coordinates are: " , coordinates)
+print(type(coordinates))
+
+#Research and bcoome up withan example of a compples data type
---
+++ lesson1c.py
@@ -0,0 +1,36 @@
+# Arithmetic opperatioons 
+#additioon
+number =10
+number2 =20
+addition =number + number2
+print("The sum of the two numbers is : ", addition)
+
+#subtraction
+number3 =50
+number4 =15
+difference =number3 - number4
+print("the difference of the numbers is" , difference)
+
+#multiplication
+number5 =10
+number6 =6
+product =number5 * number6
+print("the product of the numbers is" , product)
+
+#division
+#number7 and number8
+
+# modulus : This means the remainder when it gets subdivided with another one
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print("The modulus of the number is " ,modulus)
+#floorn division -is the number of times of a number subdivides  another ignoring the remainder
+floor =number9 //number10
+print("Thew floor division of the two  numbers is: ", floor)
+
+#Exponent
+#The power of a number 
+number11 =10
+exponent = number11 ** 2
+print("Thhe exponent of the numberf is: ",)
